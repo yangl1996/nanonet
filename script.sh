5c7ccb28a2ce13c5e51cea3ecde327f613cbe97d
Lei Yang <leiy@mit.edu>
2022-04-28 02:12:58 +0000

    change veth name and force bbr

--- script.sh
+++ script.sh
@@ -42,25 +42,25 @@ function add_virtual_interface
 	nsname="ramjet-s$sid-n$nid"
 	ip netns add $nsname
 	rootportname="s${sid}n${nid}"
-	ip link add eth0 netns $nsname type veth peer name $rootportname 
+	ip link add veth0 netns $nsname type veth peer name $rootportname 
 
 	# construct the IP address for the node and the host (physical machine)
 	# we need IP addresses on the host side because the node needs it as the gateway
 	nodeip=`id_to_nodeip $sid $nid`
 	hostip=`id_to_hostip $sid $nid`
-	ip -n $nsname addr add "$nodeip/31" dev eth0
+	ip -n $nsname addr add "$nodeip/31" dev veth0
 	ip addr add "$hostip/31" dev $rootportname
 
 	# bring up both ends of the tunnel
-	ip -n $nsname link set eth0 up
+	ip -n $nsname link set veth0 up
 	ip link set $rootportname up
 
 	# add routes to/from the node
 	ip route add $nodeip dev $rootportname
-	ip -n $nsname route add 10.0.0.0/8 via $hostip dev eth0
+	ip -n $nsname route add 10.0.0.0/8 via $hostip dev veth0
 
 	# add htb for us to classify traffic into later
-	ip netns exec $nsname tc qdisc add dev eth0 handle 1: root htb default 2
+	ip netns exec $nsname tc qdisc add dev veth0 handle 1: root htb default 2
 	tc qdisc add dev $rootportname handle 1: root htb default 2
 	echo "server $sid node $nid created at $nodeip"
 }
@@ -83,11 +83,14 @@ function rate_limit
 	nsname="ramjet-s$sid-n$nid"
 	rootportname="s${sid}n${nid}"
 	# egress rate limiting at the interface from the node to the global namespace
-	ip netns exec $nsname tc class add dev eth0 parent 1: classid 1:1 htb rate "${kbps}kbit" ceil "${kbps}kbit" burst 1540
-	ip netns exec $nsname tc class add dev eth0 parent 1:1 classid 1:2 htb rate 1kbit ceil 10gbit burst 1540
+	ip netns exec $nsname tc class add dev veth0 parent 1: classid 1:1 htb rate "${kbps}kbit" ceil "${kbps}kbit" burst 1540
+	ip netns exec $nsname tc class add dev veth0 parent 1:1 classid 1:2 htb rate 1kbit ceil 10gbit burst 1540
 	# ingress rate limiting at the interface from the global to the node namespace
 	tc class add dev $rootportname parent 1: classid 1:1 htb rate "${kbps}kbit" ceil "${kbps}kbit" burst 1540
 	tc class add dev $rootportname parent 1:1 classid 1:2 htb rate 1kbit ceil 20gbit burst 1540
+	# force use bbr so that we do not need to worry about queues
+	sysctl -w net.ipv4.tcp_congestion_control=bbr > /dev/null
+	ip netns exec $nsname sysctl -w net.ipv4.tcp_congestion_control=bbr > /dev/null
 }
 
 function add_artf_delay
@@ -100,13 +103,13 @@ function add_artf_delay
 	dstip=`id_to_nodeip $dst_sid $dst_nid`
 	nsname="ramjet-s$src_sid-n$src_nid"
 	# figure out the next available class id
-	largest_class=`ip netns exec $nsname tc class show dev eth0 | grep 'htb 1:' | sed -n 's/.*htb 1:\([0-9][0-9]*\) .*/\1/p' | sort -r | head -n1`
+	largest_class=`ip netns exec $nsname tc class show dev veth0 | grep 'htb 1:' | sed -n 's/.*htb 1:\([0-9][0-9]*\) .*/\1/p' | sort -r | head -n1`
 	next_class=$(($largest_class + 1))
 
 	# add a new class
-	ip netns exec $nsname tc class add dev eth0 parent 1:1 classid 1:$next_class htb rate 1kbit ceil 10gbit burst 1540
-	ip netns exec $nsname tc filter add dev eth0 parent 1: u32 match ip dst $dstip/32 flowid 1:$next_class
-	ip netns exec $nsname tc qdisc add dev eth0 parent 1:$next_class netem delay "${ms}ms"
+	ip netns exec $nsname tc class add dev veth0 parent 1:1 classid 1:$next_class htb rate 1kbit ceil 10gbit burst 1540
+	ip netns exec $nsname tc filter add dev veth0 parent 1: u32 match ip dst $dstip/32 flowid 1:$next_class
+	ip netns exec $nsname tc qdisc add dev veth0 parent 1:$next_class netem delay "${ms}ms"
 }
 
 function add_route_to
@@ -148,17 +151,17 @@ function set_loss
 
 	# find the qdisc we configured for this node
 	hexip=`id_to_hexnodeip $dst_sid $dst_nid`
-	filterline=`ip netns exec $nsname tc filter show dev eth0 | grep -B1 $hexip`
+	filterline=`ip netns exec $nsname tc filter show dev veth0 | grep -B1 $hexip`
 	# if there is no such qdisc, report an error
 	if [ "$?" -ne 0 ]; then
 		echo "error: no delay set from ($src_sid, $src_nid) to ($dst_sid, $dst_nid)"
 		exit 1
 	fi
 	classid=`echo $filterline | sed -n 's/.*flowid \([0-9]*:[0-9]*\).*/\1/p'`
-	classline=`ip netns exec $nsname tc class show dev eth0 classid $classid`
-	qdiscline=`ip netns exec $nsname tc qdisc show dev eth0 parent $classid`
+	classline=`ip netns exec $nsname tc class show dev veth0 classid $classid`
+	qdiscline=`ip netns exec $nsname tc qdisc show dev veth0 parent $classid`
 	currentdelay=`echo $qdiscline | sed -n 's/.*delay \([0-9]*[a-z]*\).*/\1/p'`
-	ip netns exec $nsname tc qdisc change dev eth0 parent $classid netem delay $currentdelay loss $targetloss
+	ip netns exec $nsname tc qdisc change dev veth0 parent $classid netem delay $currentdelay loss $targetloss
 }
 
 case $1 in
